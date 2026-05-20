8e342a66f8488ae7598a2ca10847c69d66201141
RexzFor <arjentinia01@gmail.com>
2026-05-20 18:49:02 +0700

    Fix problem

--- build/customer/flutter_bootstrap.js
+++ build/customer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "917458309" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1493904374" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- build/qr/flutter_bootstrap.js
+++ build/qr/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "435530266" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2348115226" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
